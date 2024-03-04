ae89e085d967422eca4f1447c1f13b388b714a06
irsyadhamdi <144642758+irsyadhamdi@users.noreply.github.com>
2024-03-04 09:57:55 +0800

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav" style="background-image: linear-gradient(pink,#2144);">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-home"></i>
+            <span>HOMEPAGE</span>
+          </a>
+        </li>
+       <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw  fa-cloud"></i>
+            <span>SERVICES</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item " href="urns.php">URNS</a> 
+            <a class="dropdown-item " href="caskets.php">CASKETS</a>
+            <a class="dropdown-item " href="vehicles.php">VEHICLES</a>
+          </div>
+        </li>-->
+        <li class="nav-item">
+          <a class="nav-link" href="Bus.php">
+            <i class="fas fa-fw fa-bus"></i>
+            <span>TRANSPORT REQUEST FORM</span></a>
+        </li>
+      </ul>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
